4de01fff7a6d97235d9514a3de5d53f2345e63b9
Rob Burtelow <rburtelow@gmail.com>
2026-03-23 09:34:01 -0500

    bumping all versions

--- talos-config-cluster/upgrade.sh
+++ talos-config-cluster/upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # qemu, scsi, linux-tools
-image="factory.talos.dev/nocloud-installer/88d1f7a5c4f1d3aba7df787c448c1d3d008ed29cfb34af53fa0df4336a56040b:v1.12.2"
+image="factory.talos.dev/nocloud-installer/88d1f7a5c4f1d3aba7df787c448c1d3d008ed29cfb34af53fa0df4336a56040b:v1.12.6"
 
 # force is used due to 2 controlplanes, can be omitted if 3 controlplanes are used
 for i in `kubectl get nodes -o wide | grep 192 | awk '{ print $6 }'`;
